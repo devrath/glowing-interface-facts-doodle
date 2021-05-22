79d7248943228a812a56e140f11b7b402cb4d650
Devrath <devrath.dev595@gmail.com>
2021-05-22 22:02:19 +0530

    m

--- Pillars of Oops/Pillars/Interface/MyYoutube/src/com/demo/VideoEncoder.java
+++ Pillars of Oops/Pillars/Interface/MyYoutube/src/com/demo/VideoEncoder.java
@@ -1,5 +1,10 @@
 package com.demo;
 
 public interface VideoEncoder {
+    float ENCODER_VALUE = 10;
     void encode(Video video);
+
+    static double someVideoLogic(double someInput){
+        return someInput + ENCODER_VALUE;
+    }
 }
