e4477ebe8f5d6d12a869e88590a0cc53be4c594a
Devrath <devrath.dev595@gmail.com>
2021-05-22 19:22:11 +0530

    c

---
+++ Pillars of Oops/Pillars/Interface/MyYoutube/src/com/demo/VideoDatabase.java
@@ -0,0 +1,5 @@
+package com.demo;
+
+public interface VideoDatabase {
+    void store(Video video);
+}
---
+++ Pillars of Oops/Pillars/Interface/MyYoutube/src/com/demo/VideoEncoder.java
@@ -0,0 +1,5 @@
+package com.demo;
+
+public interface VideoEncoder {
+    void encode(Video video);
+}
